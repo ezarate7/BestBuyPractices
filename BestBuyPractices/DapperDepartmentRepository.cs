14d4891f29d3d27095ed3e4c05aed5169843fc7c
Eddie Z <e.zarate7@hotmail.com>
2022-09-07 17:15:02 -0600

    more updates

---
+++ BestBuyPractices/DapperDepartmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBuyPractices
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        public IEnumerable<Departments> GetAllDepartments()
+        {
+            throw new NotImplementedException();
+        }
+        public void InsertDeparments()
+        {
+
+        }
+        
+        
+    }
+}
